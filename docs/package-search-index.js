9c73aaef372ef6cb0916b450315f33a788dc2177
AlexisC183 <cuartoangel1213@gmail.com>
2025-07-02 13:00:44 -0700

    Generated javadoc (true)

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.alexisc183.postgure"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.alexisc183.postgure","l":"DataContext"}];updateSearchResults();
